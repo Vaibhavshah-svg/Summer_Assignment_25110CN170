0bdb2da69937916f4fcbd832c1e484747241bfb7
Vaibhav Shah <shahvaibhav603@gmail.com>
2026-06-03 15:50:40 +0530

    Add files via upload

---
+++ Day3_4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2, a, b, temp, gcd, lcm;
+    printf("Enter two integers: ");
+    scanf("%d %d", &n1, &n2);
+    a = (n1 < 0) ? -n1 : n1;
+    b = (n2 < 0) ? -n2 : n2;
+    while (b != 0)
+    {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+    gcd = a;
+    lcm = (n1 * n2) / gcd;
+
+    printf("The LCM of %d and %d is: %d\n", n1, n2, lcm);
+
+    return 0;
+}
