6a6f15a755e24f2f76e4d1f2db19d587a86a7d04
Vaibhav Shah <shahvaibhav603@gmail.com>
2026-06-22 20:51:25 +0530

    Add files via upload

---
+++ Day22_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int i, length = 0, palindrome = 1;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    while (str[length] != '\0' && str[length] != '\n')
+    {
+        length++;
+    }
+    for (i = 0; i < length / 2; i++)
+    {
+        if (str[i] != str[length - i - 1])
+        {
+            palindrome = 0;
+            break;
+        }
+    }
+    if (palindrome)
+        printf("The string is a palindrome.\n");
+    else
+        printf("The string is not a palindrome.\n");
+    return 0;
+}
